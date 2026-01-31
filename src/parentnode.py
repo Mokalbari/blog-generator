38bcbc594d37ad76ddd60ca10a2f36130d9f0717
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2026-01-31 17:44:58 +0100

    add parent node

---
+++ src/parentnode.py
@@ -0,0 +1,13 @@
+from src.htmlnode import HTMLNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("tag is mandatory")
+
+        if self.children is None:
+            raise ValueError("children is mandatory")
