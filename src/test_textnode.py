8069b25f1d2d45e5a284393fdbc3f5c30033a863
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2026-01-31 16:04:59 +0100

    add test file

---
+++ src/test_textnode.py
@@ -0,0 +1,20 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+        node3 = TextNode("This is an italic node", TextType.ITALIC)
+        self.assertNotEqual(node, node3)
+
+        node4 = TextNode("This is another type of node", TextType.CODE)
+        self.assertNotEqual(node, node4)
+
+
+if __name__ == "__main__":
+    unittest.main()
